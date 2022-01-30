0adf86f2d63c50befcc2445c4f119bd7543a6661
Flavio Melo <fhvmelo@gmail.com>
2022-01-30 20:09:40 -0300

    Aula 1.3 - Servlet
    - criando uma classe com servlet (classes java para dinamizar as aplicações): criação e edição da classe servlet, anotação @WebServlet para mapear o padrão de url, sobrescrever o método service (que é responsável por receber uma requisição e tratá-la).
    - exibindo objetos para o usuário: criação de uma lista de objetos para exibir ao usuário; passar um parâmetro na url via método get.

---
+++ CatalogoDeFilmes/src/main/java/br/com/lead/modelo/Filme.java
@@ -0,0 +1,25 @@
+package br.com.lead.modelo;
+
+public class Filme {
+	private String nome;
+	private String genero;
+	private Integer ano;
+	
+	public Filme(String nome, String genero, Integer ano) {
+		this.nome = nome;
+		this.genero = genero;
+		this.ano = ano;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getGenero() {
+		return this.genero;
+	}
+	
+	public Integer getAno() {
+		return this.ano;
+	}
+}
---
+++ CatalogoDeFilmes/src/main/java/br/com/lead/servlet/FilmeServlet.java
@@ -0,0 +1,54 @@
+package br.com.lead.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.lead.modelo.Filme;
+
+@WebServlet("/filme")
+public class FilmeServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Filme coringa = new Filme("Coringa", "Drama", 2019);
+		Filme matrix = new Filme("Matrix", "Ação", 1999);
+		Filme forrestGump =new Filme("Forrest Gump", "Drama", 1994);
+		
+		ArrayList<Filme> filmes = new ArrayList<Filme>();
+		
+		filmes.add(coringa);
+		filmes.add(matrix);
+		filmes.add(forrestGump);
+		
+		req.getParameter("genero");
+		String genero = req.getParameter("genero");
+		
+		resp.setContentType("text/HTML");
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<h2> Lista de Filmes, utilizando Servlet </h2>");
+		
+		out.println("<ol>");
+		/*
+		filmes.forEach(filme -> {
+			out.println(String.format("<li> Nome: %s", filme.getNome()));
+			out.println(String.format("Gênero: %s", filme.getGenero()));
+			out.println(String.format("Ano: %s </li>", filme.getAno()));
+		});
+		*/
+		//requisição do tipo get (com parâmetros passados na url) para filtrar os filmes com base no gênero
+		filmes.stream().filter(filme -> filme.getGenero().toUpperCase().equals(genero.toUpperCase())).forEach(filme -> {
+			out.println(String.format("<li> Nome: %s", filme.getNome()));
+			out.println(String.format("Gênero: %s", filme.getGenero()));
+			out.println(String.format("Ano: %s </li>", filme.getAno()));
+		});
+		out.println("</ol>");
+		out.close();
+	}
+}
